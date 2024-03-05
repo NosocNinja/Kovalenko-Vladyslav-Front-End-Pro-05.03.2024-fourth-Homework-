139b9afdfab17370ea0a4de98a270b75a1aa5fb9
Vlad <vlkovalenk@gmail.com>
2024-03-05 21:00:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let question = prompt('How many hours? (not now, only numbers)');
+let answer = 0;
+const seconds = 3600;
+
+Number(question);
+answer = question * seconds;
+
+alert(addNumbers + ' seconds');
